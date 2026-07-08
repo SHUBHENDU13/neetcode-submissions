319393069fb42daaa6cc5359f8ec975dee9db35d
SHUBHENDU13 <97780453+SHUBHENDU13@users.noreply.github.com>
2026-07-08 08:02:40 +0530

    Add: min-cost-to-connect-points - submission-1

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        adj = {}
+        for i in range(len(points)):
+            adj[i] = []
+
+        for i in range(len(points)):
+            for j in range(len(points)):
+                if j == i:
+                    continue
+                distance = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                adj[i].append([j, distance])
+
+        visit = set()
+        minHeap = []
+
+        visit.add(0)
+        for neighbour, weight in adj[0]:
+            heapq.heappush(minHeap, [weight, 0, neighbour])
+
+        res = 0
+
+        while minHeap:
+            weight, src, node = heapq.heappop(minHeap)
+            if node in visit:
+                continue
+
+            visit.add(node)
+            res += weight
+
+            for neighbour, weight in adj[node]:
+                if neighbour not in visit:
+                    heapq.heappush(minHeap, [weight, node, neighbour])
+
+        return res
