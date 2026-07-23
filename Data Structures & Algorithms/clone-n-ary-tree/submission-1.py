f6804bb9e5765be408d4d95e4763f5161daa02a4
SHUBHENDU13 <97780453+SHUBHENDU13@users.noreply.github.com>
2026-07-23 08:08:42 +0530

    Add: clone-n-ary-tree - submission-1

---
+++ Data Structures & Algorithms/clone-n-ary-tree/submission-1.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def cloneTree(self, root: 'Node') -> 'Node':
+        
+        def dfs(node):
+            if not node:
+                return
+            new_node = Node()
+            new_node.val = node.val
+            
+            for child in node.children:
+                new_node.children.append(dfs(child))
+
+            return new_node
+
+        return dfs(root)
+
