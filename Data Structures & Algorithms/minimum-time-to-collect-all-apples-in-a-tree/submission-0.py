afdb33409fb55aa38485348fa48aa844670cdf88
SHUBHENDU13 <97780453+SHUBHENDU13@users.noreply.github.com>
2026-07-24 19:06:09 +0530

    Add: minimum-time-to-collect-all-apples-in-a-tree - submission-0

---
+++ Data Structures & Algorithms/minimum-time-to-collect-all-apples-in-a-tree/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        
+        adj = {}
+        for i in range(n):
+            adj[i] = []
+        for src, dst in edges:
+            adj[src].append(dst)
+            adj[dst].append(src)
+
+        visit = set()
+        def dfs(node):
+            time = 0
+            visit.add(node)
+            for child in adj[node]:
+                if child in visit:
+                    continue
+                childTime = dfs(child)
+                if childTime or hasApple[child]:
+                    time += 2 + childTime
+
+            return time
+
+        return dfs(0)
+
+
+            
+
+            
