efc108195d3d2a34377c8cd1a09a9d83fca70319
SHUBHENDU13 <97780453+SHUBHENDU13@users.noreply.github.com>
2026-07-26 11:31:26 +0530

    Add: sentence-similarity - submission-0

---
+++ Data Structures & Algorithms/sentence-similarity/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        if len(sentence1) != len(sentence2):
+            return False
+
+        hash_map = defaultdict(set)
+
+        for word1, word2 in similarPairs:
+            hash_map[word1].add(word2)
+            hash_map[word2].add(word1)
+
+        for i in range(len(sentence1)):
+            if sentence1[i] != sentence2[i] and sentence2[i] not in hash_map[sentence1[i]]:
+                return False
+        return True
