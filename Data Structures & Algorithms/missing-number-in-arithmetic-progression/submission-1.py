b1a540a17dc89b55760465004b293b89d3546fd1
SHUBHENDU13 <97780453+SHUBHENDU13@users.noreply.github.com>
2026-07-29 05:59:04 +0530

    Add: missing-number-in-arithmetic-progression - submission-1

---
+++ Data Structures & Algorithms/missing-number-in-arithmetic-progression/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, arr: List[int]) -> int:
+        
+        diff = (arr[-1] - arr[0])//len(arr)
+
+        for i in range(len(arr)-1):
+            if arr[i+1] != arr[i] + diff:
+                return arr[i] + diff
+        
+        return arr[0]
