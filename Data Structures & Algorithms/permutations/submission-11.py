e0fd880f74534b76daccd42bb75efb605ff7980b
SHUBHENDU13 <97780453+SHUBHENDU13@users.noreply.github.com>
2026-08-27 08:08:29 +0530

    Add: permutations - submission-11

---
+++ Data Structures & Algorithms/permutations/submission-11.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        comb = []
+
+        def backtrack(i):
+            if i == len(nums):
+                res.append(comb.copy())
+                return
+
+            for j in range(len(comb) + 1):
+                comb.insert(j, nums[i])
+                backtrack(i + 1)
+                comb.pop(j)
+        
+        backtrack(0)
+        return res
