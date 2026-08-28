ad510a5d79d6cba0b88841ebf23969b45174ed7b
SHUBHENDU13 <97780453+SHUBHENDU13@users.noreply.github.com>
2026-08-28 15:20:16 +0530

    Add: factor-combinations - submission-5

---
+++ Data Structures & Algorithms/factor-combinations/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def getFactors(self, n: int) -> List[List[int]]:
+        ans = []
+        self.backtrack([n], ans)
+        return ans
+
+    def backtrack(self, factors, ans):
+        if len(factors) > 1:
+            ans.append(factors.copy())
+
+        last_factor = factors.pop()
+
+        i = 2 if not factors else factors[-1]
+
+        while i <= last_factor // i:
+            if last_factor % i == 0:
+                factors.append(i)
+                factors.append(last_factor//i)
+                self.backtrack(factors, ans)
+                factors.pop()
+                factors.pop()
+            i += 1
+
+        factors.append(last_factor)
+        
