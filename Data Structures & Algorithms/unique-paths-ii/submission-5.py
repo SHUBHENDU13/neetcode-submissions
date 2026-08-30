6410183baee65d584729108ef91505a7e953f1ce
SHUBHENDU13 <97780453+SHUBHENDU13@users.noreply.github.com>
2026-08-30 19:03:55 +0530

    Add: unique-paths-ii - submission-5

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        ROWS, COLS = len(obstacleGrid), len(obstacleGrid[0])
+        cache = {}
+        
+        def dp(r, c):
+            if (r >= ROWS or c >= COLS or obstacleGrid[r][c] == 1):
+                return 0
+
+            if (r == ROWS - 1 and c == COLS - 1):
+                return 1
+
+            if (r, c) in cache:
+                return cache[(r, c)]
+
+            right = dp(r, c + 1)
+            bot = dp(r + 1, c)
+            cache[(r, c)] = right + bot
+            return cache[(r, c)]
+
+        return dp(0,0)
