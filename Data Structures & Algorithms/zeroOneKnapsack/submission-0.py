dbaf593f53aa59d8b9423d79c47ab0dfd8a3ebfb
SHUBHENDU13 <97780453+SHUBHENDU13@users.noreply.github.com>
2026-09-03 16:33:48 +0530

    Add: zeroOneKnapsack - submission-0

---
+++ Data Structures & Algorithms/zeroOneKnapsack/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumProfit(self, profit: List[int], weight: List[int], capacity: int) -> int:
+
+        cache = {}
+
+        def dp(i, capacity):
+            if i == len(weight):
+                return 0
+
+            if (i, capacity) in cache:
+                return cache[(i, capacity)]
+
+            maxProfit = dp(i + 1, capacity)
+            newCap = capacity - weight[i]
+            if newCap >= 0:
+                p = profit[i] + dp(i + 1, newCap)
+                maxProfit = max(maxProfit, p)
+            cache[(i, capacity)] = maxProfit
+            return cache[(i, capacity)]
+
+        return dp(0,capacity)
