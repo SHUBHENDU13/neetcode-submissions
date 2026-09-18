d367c5e66ea6a386aab43f24b3356df8c445f39f
SHUBHENDU13 <97780453+SHUBHENDU13@users.noreply.github.com>
2026-09-18 07:10:16 +0530

    Add: course-schedule - submission-11

---
+++ Data Structures & Algorithms/course-schedule/submission-11.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        for crs, pre in prerequisites:
+            adj[crs].append(pre)
+        
+        cache = {}
+
+        # visit = set()
+        path = set()
+
+        def dfs(crs):
+            if crs in path:
+                return False
+            if crs in cache:
+                return cache[crs]
+            # if crs in visit:
+            #     return True
+
+            # visit.add(crs)
+            path.add(crs)
+            tmp = True
+            for nei in adj[crs]:
+                if not dfs(nei):
+                    tmp = False
+            path.remove(crs)
+            cache[crs] = tmp
+            return cache[crs]
+
+        for i in range(numCourses):
+            if not dfs(i): return False
+        return True
+        
