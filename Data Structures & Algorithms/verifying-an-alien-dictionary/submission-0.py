3e6b1237aa68b278bbff5e726fc21a7e0f243963
SHUBHENDU13 <97780453+SHUBHENDU13@users.noreply.github.com>
2026-09-19 12:46:26 +0530

    Add: verifying-an-alien-dictionary - submission-0

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        adj = defaultdict(list)
+        for i in range(len(order)-1):
+            c1, c2 = order[i], order[i + 1]
+            adj[c1].append(c2)
+
+        def dfs(c, target, visit):
+            if c == target:
+                return True
+            if c in visit:
+                return False
+
+            visit.add(c)
+            for nei in adj[c]:
+                if dfs(nei, target, visit): return True
+            return False          
+        
+        for i in range(len(words)-1):
+            w1, w2 = words[i], words[i + 1]
+            minLen = min(len(w1), len(w2))
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    if not dfs(w1[j], w2[j], set()): return False
+                    break
+            else:
+                if len(w1) > len(w2):
+                    return False
+        
+        return True
+
